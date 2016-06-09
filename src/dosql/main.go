802b7077ea50ef574e6d3e5c8b95a7244d054df8
Kyl Wellman <kyl@gratafy.com>
2016-06-09 10:30:17 -0700

    catch commands that could mutate data, and return an error if a -f flag was not set

--- src/dosql/main.go
+++ src/dosql/main.go
@@ -7,21 +7,27 @@ import (
 	"github.com/jmoiron/sqlx"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 const (
 	usage = `
 Usage:
-	dosql [ -F configFile ] [ -e environment ] [<script>]
+	dosql [ -F configFile ] [ -e environment ] [ -f ] [<script>]
 
 Arguments:
 	<script>  The script to run.  If it is not provided, reads from stdin
 
 Options:
-	-e=env   Configuration environment [default: default]
-	-F=file  Configuration file [default: /usr/local/etc/dosql/config.toml]
+	-e=env    Configuration environment [default: default]
+	-F=file   Configuration file [default: /usr/local/etc/dosql/config.toml]
+	-f        Use this flag to enable commands that mutate data
 `
 	version = `dosql 0.0.1`
+)
+
+var (
+	unsafeKeywords = [...]string{"update", "delete", "insert", "truncate", "alter", "drop", "create"}
 )
 
 func main() {
@@ -57,6 +63,18 @@ func main() {
 		script = string(b[:])
 	}
 
+	if force, ok := dict["-f"].(bool); !ok || !force {
+		isSafe := scriptIsSafe(script)
+		if !isSafe {
+			fmt.Printf("Script contained commands that could mutate data, and -f was not set\n")
+			os.Exit(1)
+		}
+	}
+
+	queryAndPrint(db, script)
+}
+
+func queryAndPrint(db *sqlx.DB, script string) {
 	rows, err := db.Queryx(script)
 	if err != nil {
 		fmt.Printf("Failed to execute query: %s\n", err)
@@ -85,4 +103,17 @@ func main() {
 		}
 		fmt.Printf("\n")
 	}
+}
+
+func scriptIsSafe(script string) bool {
+	lowerScript := strings.ToLower(script)
+
+	for _, k := range unsafeKeywords {
+		if strings.Contains(lowerScript, k) {
+			return false
+		}
+	}
+
+	return true
+
 }
